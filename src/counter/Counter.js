78f05e516406c6655e11f21cad9b32b424508cb7
ritabenyona <ritabenyona>
2019-05-04 19:51:39 -0400

    commit the message

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -3,37 +3,35 @@ import { Form, TextArea, Label } from "semantic-ui-react";
 import { Space } from "aws-amplify-react/dist/AmplifyTheme";
 import Amplify, { API, graphqlOperation, Storage } from "aws-amplify";
 
-
- const UpdatePhoto = `mutation UpdatePhoto($id: ID!, $score: Int) {
+const UpdatePhoto = `mutation UpdatePhoto($id: ID!, $score: Int) {
 	updatePhoto(input:{id: $id, score: $score}){
   	id
     score
 	}
-}`
+}`;
 
 const GetPhoto = `query GetPhoto($id: ID!) {
 	getPhoto(id: $id){
   	id
     score
 	}
-}`
+}`;
 
 const CreateComment = `mutation CreateComment($text: String!, $commentPhotoId: ID!) {
 	createComment(input:{text: $text, commentPhotoId: $commentPhotoId}){
   	id
     text
 	}
-}`
+}`;
 
 class Counter extends Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-			count: 0,
-			albumName: "",
-			textAreaValue: ""
-		};
-
+      count: 0,
+      albumName: "",
+      textAreaValue: ""
+    };
   }
 
   styles = {
@@ -42,15 +40,15 @@ class Counter extends Component {
   };
 
   handleIncrement = async _product => {
-		const result  = await API.graphql(
+    const result = await API.graphql(
       graphqlOperation(UpdatePhoto, {
         id: this.props.photoId,
         score: this.state.count + 1
       })
-		);
-		this.setState({ count: result.data.updatePhoto.score });
-		console.log("Increment Clicked", this);
-		console.log("After update the score in DB" + result.data.updatePhoto.score);
+    );
+    this.setState({ count: result.data.updatePhoto.score });
+    console.log("Increment Clicked", this);
+    console.log("After update the score in DB" + result.data.updatePhoto.score);
   };
   handleOnChange(event) {
     this.setState({
@@ -61,37 +59,40 @@ class Counter extends Component {
     if (this.state.images.length === 0) return <p>There are no images'</p>;
     else
       return (
-        <ul>
+        <ul style="width:10">
           {this.state.images.map(tag => (
             <li key={tag}>{tag}</li>
           ))}
         </ul>
       );
-	}
+  }
 
-	async getScore(){
-		const result  = await API.graphql(
+  async getScore() {
+    const result = await API.graphql(
       graphqlOperation(GetPhoto, {
         id: this.props.photoId
       })
-		);
-		console.log("Getting the latest score ", result)
-		this.setState({ count: result.data.getPhoto.score });
-	}
+    );
+    console.log("Getting the latest score ", result);
+    this.setState({ count: result.data.getPhoto.score });
+  }
 
-	async componentWillUnmount()
-	{
-		const result  = await API.graphql(
+  async componentWillUnmount() {
+    const result = await API.graphql(
       graphqlOperation(CreateComment, {
-        text: this.state.textAreaValue, commentPhotoId: this.props.photoId
+        text: this.state.textAreaValue,
+        commentPhotoId: this.props.photoId
       })
-		);
-		console.log("That's what we have in the comments ", this.state.textAreaValue)
-		console.log("Result is ", result)
-	}
+    );
+    console.log(
+      "That's what we have in the comments ",
+      this.state.textAreaValue
+    );
+    console.log("Result is ", result);
+  }
 
   render() {
-		this.state.count === 0? this.getScore(): void 0;
+    this.state.count === 0 ? this.getScore() : void 0;
     return (
       <React.Fragment>
         <Form>
